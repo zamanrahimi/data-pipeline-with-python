d0b583080c14077c99ba5ac53dacf5a503d49f2d
User1 <user1@User1s-MacBook-Pro.local>
2024-08-07 22:15:17 -0500

    project folder restructured

--- pipeline/data_pipeline.py
+++ pipeline/data_pipeline.py
@@ -3,20 +3,24 @@ import pandas as pd
 import boto3
 from io import StringIO
 
+
 def fetch_data(api_url):
     response = requests.get(api_url)
     response.raise_for_status()
     return response.json()
 
+
 def process_data(data):
     df = pd.DataFrame(data)
     return df
 
+
 def save_data_to_s3(df, bucket_name, file_name):
     csv_buffer = StringIO()
     df.to_csv(csv_buffer, index=False)
-    s3_resource = boto3.resource('s3')
+    s3_resource = boto3.resource("s3")
     s3_resource.Object(bucket_name, file_name).put(Body=csv_buffer.getvalue())
+
 
 if __name__ == "__main__":
     API_URL = "https://jsonplaceholder.typicode.com/users"
